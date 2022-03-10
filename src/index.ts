4f75623140aaaef7f21b00eebd60cf4d3f8a9537
Remyto <remycherkaoui@gmail.com>
2022-03-10 13:05:53 +0100

    npm run dev

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,26 @@
+import { application } from "express";
 import { fastify } from "./lib/fastify";
 
 const PORT = process.env.PORT || 5000;
 
 // Routes
-fastify.get("/", (req, reply) => {
-  reply.send({ test: "Hello World" });
-});
 
-// Start the server
-const start = async () => {
+// Listen
+/* fastify.listen(process.env.PORT ?? 3000).catch(console.error) */
+application.listen(PORT, () => {
+  console.log(
+    `Serrrver running in ${process.env.NODE_ENV} mode on port ${PORT}`
+  );
+});
+/* const start = async () => {
   try {
     await fastify.listen(PORT);
     fastify.log.info(
-      `Server running in ${process.env.NODE_ENV} mode on port port ${PORT}`
+      `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
     );
   } catch (error) {
     fastify.log.error(error);
     process.exit(1);
   }
 };
-start();
-// fastify.listen(process.env.PORT ?? 3000).catch(console.error)
+start(); */
